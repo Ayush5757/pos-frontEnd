5094bdfc4721a4e4a27ab16d05c64dfda0a31d36
Aayush Mishra <ayushmishra5757@gmail.com>
2025-05-07 18:52:54 +0530

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.vite_web_socket_backend, 
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // env variables ko load karo
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_WEB_SOCKET_BACKEND,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/socket.io': {
+          target: env.VITE_WEB_SOCKET_BACKEND,
+          ws: true,
+          changeOrigin: true,
+        },
       },
-      '/socket.io': {
-        target: import.meta.env.vite_web_socket_backend,
-        ws: true,
-        changeOrigin: true,
-      }
-    }
-  }
+    },
+  };
 });
